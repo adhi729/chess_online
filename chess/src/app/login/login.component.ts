5c8c60dc9a42cf004806c6399abd8934493f2183
adhi729 <adhi.adhiyaman@gmail.com>
2018-03-27 22:58:15 +0530

    front end login component redirect

--- chess/src/app/login/login.component.ts
+++ chess/src/app/login/login.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Router} from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+	
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
+loginUser(e):void{
+		e.preventDefault();
+		var userid = e.target.elements[0].value;
+		var pass = e.target.elements[1].value;
 
+		if (userid=="user" && pass =="user"){
+			this.router.navigate(['home']);
+		}
+	}
 }
