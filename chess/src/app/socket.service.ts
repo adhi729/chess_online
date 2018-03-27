47668627173d7137112cef06d99ac91103d1dbf7
adhi729 <adhi.adhiyaman@gmail.com>
2018-03-28 01:57:27 +0530

    service on sockets

---
+++ chess/src/app/socket.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Observer } from 'rxjs/Observer';
+import { Message } from './class_defs/message_item';
+
+import * as socketIo from 'socket.io-client';
+
+const SERVER_URL =  'http://localhost:3000';
+
+@Injectable()
+export class SocketService {
+	private Socket;
+
+	public initSocket(): void{
+		this.Socket = socketIo(SERVER_URL);
+	}
+	public sendMessage(message: Message): void{
+		this.Socket.emit('message', message);
+	}
+	public onMessage(): Observable<Message>{
+		return new Observable<Message>(Observer => {
+			this.Socket.on('message', (data: Message) => Observer.next(data));
+		});
+	}
+
+  constructor() { }
+
+}
