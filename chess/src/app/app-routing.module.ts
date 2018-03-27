69969004868cf0ff6862a5a89b55b5aaa5f04059
adhi729 <adhi.adhiyaman@gmail.com>
2018-03-27 21:46:32 +0530

    default == login page
    on login delete login page
    routerlink deploys app

--- chess/src/app/app-routing.module.ts
+++ chess/src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './login/login.component';
+import { DummyComponent } from './dummy/dummy.component';
 
 const routes : Routes = [
-	{path: 'home', component: LoginComponent}
+	{path: 'home', component: DummyComponent}
 ]
 
 @NgModule({
