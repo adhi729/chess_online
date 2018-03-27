3e2a22bcaac3116deb4516259d48106e8b315f23
adhi729 <adhi.adhiyaman@gmail.com>
2018-03-28 00:52:29 +0530

    removed package io from login.ts
    included it in script src

--- chess/src/app/login/login.component.ts
+++ chess/src/app/login/login.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserLoginService } from '../user-login.service';
-import io from "socket.io-client";
 
 @Component({
   selector: 'app-login',
